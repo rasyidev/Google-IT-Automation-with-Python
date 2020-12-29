5a1bffcd7f88bfbd5f6c0643a4716d8dd3f19cbd
rasyidev <habib.rasyid11@gmail.com>
2020-12-29 22:00:22 +0700

    Module 3 Assessment is DONE

--- Course 1 - Crash Course on Python/15-sum-the-positive-numbers.py
+++ Course 1 - Crash Course on Python/15-sum-the-positive-numbers.py
@@ -2,6 +2,7 @@
 Implement the sum_positive_numbers function, as a recursive function that returns the sum of all positive numbers between the number n received and 1. For example, when n is 3 it should return 1+2+3=6, and when n is 5 it should return 1+2+3+4+5=15.
 '''
 
+
 def sum_positive_numbers(n):
   if n == 1:
     return 1
